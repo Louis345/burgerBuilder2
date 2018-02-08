978679ddc2e703f184201d14b8790fc6e60fcc3e
Jamal Taylor <jamal.taylor@cengage.com>
2018-02-08 16:46:54 -0500

    Added a new BuildControl Folder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
-import Burger from "../../components/Burger/Burger";
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 1,
       bacon: 1,
       cheese: 2,
-      meat: 2
+      meat: 3
     }
   };
   render() {
